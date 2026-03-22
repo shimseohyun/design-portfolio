c30aec420781ed5970146a7a78e530e90e62c981
shimseohyun <shim020924@gmail.com>
2026-03-22 11:51:36 +0900

    [Feat] #1 - add style, router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@V01/layouts": path.resolve(__dirname, "src/v01/layouts"),
+      "@V01/pages": path.resolve(__dirname, "src/v01/pages"),
+      "@V01/router": path.resolve(__dirname, "src/v01/router"),
+    },
+  },
+});
